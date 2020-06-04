9691fa5535847148ab20db52240a5ca07d7244d8
Ganza respice <umujyi2019@gmail.com>
2020-06-04 11:17:07 +0200

    ..

--- resources/views/front/common/enterprise-card.blade.php
+++ resources/views/front/common/enterprise-card.blade.php
@@ -36,7 +36,6 @@
                     </h6>
                     <hr>
                     
-                
                     <p class="mbr-text mbr-fonts-style display-7">
                      
                         <?php $end = ' <a  href="/enterprises/view/'.$enterprise->id.'" target="_blank">  •••</a>'; ?>
