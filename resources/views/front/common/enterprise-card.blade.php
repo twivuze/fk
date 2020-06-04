86827b893d529156c1399166d159ceb32543e802
Ganza respice <umujyi2019@gmail.com>
2020-06-04 11:10:14 +0200

    ..

--- resources/views/front/common/enterprise-card.blade.php
+++ resources/views/front/common/enterprise-card.blade.php
@@ -39,9 +39,9 @@
                 
                     <p class="mbr-text mbr-fonts-style display-7">
                      
-                        <?php $end = ' <a  href="/enterprises/view/'.$enterprise->id.'" target="_blank"> read more ....</a>'; ?>
+                        <?php $end = ' <a  href="/enterprises/view/'.$enterprise->id.'" target="_blank">  ....</a>'; ?>
 
-                        {!!html_entity_decode(Str::limit($enterprise->fundraising_message, $limit = 140,$end))!!}
+                        {!!html_entity_decode(Str::limit($enterprise->fundraising_message, $limit = 100,$end))!!}
                     </p>
                     <?php if(intval($enterprise->lender_initial_target) > 0){?>
                         
