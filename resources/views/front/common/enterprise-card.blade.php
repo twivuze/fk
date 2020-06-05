d118e9390e193293f02da11b1cbb4e6d4208e0e5
Ganza respice <umujyi2019@gmail.com>
2020-06-05 15:16:10 +0200

    ..

--- resources/views/front/common/enterprise-card.blade.php
+++ resources/views/front/common/enterprise-card.blade.php
@@ -28,7 +28,7 @@
                  
                    <h4 class="card-title pb-3 mbr-fonts-style display-7 text-center">
                    <?php $end1 = ' <a  href="/enterprises/view/'.$enterprise->id.'" target="_blank">  •••</a>'; ?>
-                        {{Str::limit($enterprise->business_name, $limit = 10,$end1) }}
+                   {!!html_entity_decode(Str::limit($enterprise->business_name, $limit = 10,$end1))!!}
 
                     </h4>
                     <h6 class="card-title pb-3 mbr-fonts-style display-7 text-center">
